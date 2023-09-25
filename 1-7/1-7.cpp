0d6accb9cb3e5f937caba1c57ab85f2ca6d4fe6b
Jeong tae yeon <jtyk119@naver.com>
2023-09-25 16:46:46 +0900

    1-7 마우스, 키보드 입력 기본 세팅

--- 1-7/1-7.cpp
+++ 1-7/1-7.cpp
@@ -5,49 +5,44 @@
 #include <gl/glew.h>
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
-#include <gl/glut.h>
 #include "filetobuf.h"
-
 
 void make_vertexShaders();
 void make_fragmentShaders();
 GLuint make_shaderProgram();
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+void Mouse(int button, int state, int x, int y);
 
 GLint width, height;
 GLuint shaderProgramID; //--- 세이더 프로그램 이름
 GLuint vertexShader; //--- 버텍스 세이더 객체
 GLuint fragmentShader; //--- 프래그먼트 세이더 객체
 
-void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+void main(int argc, char** argv)
 {
-	width = 500;
-	height = 500;
+	width = 600;
+	height = 600;
 	//--- 윈도우 생성하기
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowPosition(200, 30);
 	glutInitWindowSize(width, height);
-	glutCreateWindow("Example1");
-	//--- GLEW 초기화하기
+	glutCreateWindow("Example");
 	glewExperimental = GL_TRUE;
 	glewInit();
-	//--- 세이더 읽어와서 세이더 프로그램 만들기
-	make_vertexShaders(); //--- 버텍스 세이더 만들기
-	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	make_vertexShaders();
+	make_fragmentShaders();
 	shaderProgramID = make_shaderProgram();
-	//--- 세이더 프로그램 만들기
-	glutDisplayFunc(drawScene); //--- 출력 콜백 함수
+	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutMainLoop();
 }
 
-GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
-{
+GLvoid drawScene() {
 	GLfloat rColor, gColor, bColor;
-	rColor = gColor = 0.0;
-	bColor = 1.0; //--- 배경색을 파랑색으로 설정
+	rColor = gColor = bColor = 1.0;
 	glClearColor(rColor, gColor, bColor, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(shaderProgramID);
@@ -56,16 +51,13 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	glutSwapBuffers(); // 화면에 출력하기
 }
 
-GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
-{
+GLvoid Reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 }
 
 void make_vertexShaders()
 {
 	GLchar* vertexSource;
-	//--- 버텍스 세이더 읽어 저장하고 컴파일 하기
-	//--- filetobuf: 사용자정의 함수로 텍스트를 읽어서 문자열에 저장하는 함수
 	vertexSource = filetobuf("vertex.glsl");
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
@@ -84,8 +76,7 @@ void make_vertexShaders()
 void make_fragmentShaders()
 {
 	GLchar* fragmentSource;
-	//--- 프래그먼트 세이더 읽어 저장하고 컴파일하기
-	fragmentSource = filetobuf("fragment.glsl"); // 프래그세이더 읽어오기
+	fragmentSource = filetobuf("fragment.glsl");
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
@@ -117,9 +108,37 @@ GLuint make_shaderProgram()
 		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
 		return false;
 	}
-	glUseProgram(shaderID); //--- 만들어진 세이더 프로그램 사용하기
-	//--- 여러 개의 세이더프로그램 만들 수 있고, 그 중 한개의 프로그램을 사용하려면
-	//--- glUseProgram 함수를 호출하여 사용 할 특정 프로그램을 지정한다.
-	//--- 사용하기 직전에 호출할 수 있다.
+	glUseProgram(shaderID);
 	return shaderID;
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'p':    // 점 그리기
+		break;
+	case 'l':    // 선 그리기
+		break;
+	case 't':    // 삼각형 그리기
+		break;
+	case 'r':    // 네모 그리기
+		break;
+	case 'w':    // 위로 이동
+		break;
+	case 'a':    // 왼쪽으로 이동
+		break;
+	case 's':    // 아래로 이동
+		break;
+	case 'd':    // 오른쪽으로 이동
+		break;
+	case 'c':   // 모든 도형 삭제
+		break;
+	case 'q':   // 프로그램 종료
+		std::cout << "프로그램을 종료합니다." << std::endl;
+		glutLeaveMainLoop();
+		break;
+	}
+}
+
+void Mouse(int button, int state, int x, int y) {
+
 }
