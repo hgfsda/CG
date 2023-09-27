7ac3b3aad40c810db4ec10ee44912c2f13f53d6b
Jeong tae yeon <jtyk119@naver.com>
2023-09-27 13:11:14 +0900

    1-7 도형 생성 기본 세팅

--- 1-7/1-7.cpp
+++ 1-7/1-7.cpp
@@ -9,59 +9,125 @@
 
 void make_vertexShaders();
 void make_fragmentShaders();
-GLuint make_shaderProgram();
+void make_shaderProgram();
+void InitBuffer();
+void reset();
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 void Mouse(int button, int state, int x, int y);
 
-GLint width, height;
 GLuint shaderProgramID; //--- 세이더 프로그램 이름
 GLuint vertexShader; //--- 버텍스 세이더 객체
 GLuint fragmentShader; //--- 프래그먼트 세이더 객체
+int cnt, save_draw_check[10];   // 현재 그려진 도형 개수, 정해진 도형 저장
+float r[10], g[10], b[10];
+GLfloat Shape[10][12];    // 도형의 좌표
+GLfloat colors[10][12];  // 도형 꼭지점의 색상
+GLuint vao, vbo[10];
+
+void reset() {
+	cnt = 0;
+	for (int i = 0; i < 10; ++i) {
+		save_draw_check[i] = 0;
+		r[i] = (rand() % 101) * 0.01, g[i] = (rand() % 101) * 0.01, b[i] = (rand() % 101) * 0.01;
+		for (int j = 0; j < 12; ++j) {
+			Shape[i][j] = 0;
+			if (j % 3 == 0) colors[i][j] = r[i];
+			else if (j % 3 == 1) colors[i][j] = g[i];
+			else if (j % 3 == 2) colors[i][j] = b[i];
+		}
+	}
+}
 
 void main(int argc, char** argv)
 {
-	width = 600;
-	height = 600;
+	srand(time(NULL));
 	//--- 윈도우 생성하기
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(200, 30);
-	glutInitWindowSize(width, height);
+	glutInitWindowSize(800, 600);
 	glutCreateWindow("Example");
 	glewExperimental = GL_TRUE;
+	reset();
 	glewInit();
-	make_vertexShaders();
-	make_fragmentShaders();
-	shaderProgramID = make_shaderProgram();
+	make_shaderProgram();
+	InitBuffer();
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutMainLoop();
 }
 
 GLvoid drawScene() {
-	GLfloat rColor, gColor, bColor;
-	rColor = gColor = bColor = 1.0;
-	glClearColor(rColor, gColor, bColor, 1.0f);
+	glClearColor(1.0, 1.0, 1.0, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(shaderProgramID);
-	glPointSize(5.0);
-	glDrawArrays(GL_POINTS, 0, 1); //--- 렌더링하기: 0번 인덱스에서 1개의 버텍스를 사용하여 점 그리기
-	glutSwapBuffers(); // 화면에 출력하기
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shaderProgramID);
+	glBindVertexArray(vao);
+	//--- 삼각형 그리기
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glutSwapBuffers();
 }
 
 GLvoid Reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 }
 
+void InitBuffer()
+{
+	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
+	glBindVertexArray(vao); //--- VAO를 바인드하기
+	glGenBuffers(2, vbo); //--- 2개의 VBO를 지정하고 할당하기
+	//--- 1번째 VBO를 활성화하여 바인드하고, 버텍스 속성 (좌표값)을 저장
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	//--- 변수 diamond 에서 버텍스 데이터 값을 버퍼에 복사한다.
+	//--- triShape 배열의 사이즈: 9 * float
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Shape, GL_STATIC_DRAW);
+	//--- 좌표값을 attribute 인덱스 0번에 명시한다: 버텍스 당 3* float
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//--- attribute 인덱스 0번을 사용가능하게 함
+	glEnableVertexAttribArray(0);
+	//--- 2번째 VBO를 활성화 하여 바인드 하고, 버텍스 속성 (색상)을 저장
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	//--- 변수 colors에서 버텍스 색상을 복사한다.
+	//--- colors 배열의 사이즈: 9 *float 
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
+	//--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//--- attribute 인덱스 1번을 사용 가능하게 함.
+	glEnableVertexAttribArray(1);
+}
+
+void make_shaderProgram()
+{
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+
 void make_vertexShaders()
 {
-	GLchar* vertexSource;
 	vertexSource = filetobuf("vertex.glsl");
+	//--- 버텍스 세이더 객체 만들기
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
 	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
 	GLint result;
 	GLchar errorLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
@@ -75,52 +141,38 @@ void make_vertexShaders()
 
 void make_fragmentShaders()
 {
-	GLchar* fragmentSource;
 	fragmentSource = filetobuf("fragment.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
 	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
 	GLint result;
 	GLchar errorLog[512];
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
 	if (!result)
 	{
 		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: frag_shader 컴파일 실패\n" << errorLog << std::endl;
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
 		return;
 	}
-}
-
-GLuint make_shaderProgram()
-{
-	GLuint shaderID;
-	GLint result;
-	GLchar errorLog[512];
-	shaderID = glCreateProgram(); //--- 세이더 프로그램 만들기
-	glAttachShader(shaderID, vertexShader); //--- 세이더 프로그램에 버텍스 세이더 붙이기
-	glAttachShader(shaderID, fragmentShader); //--- 세이더 프로그램에 프래그먼트 세이더 붙이기
-	glLinkProgram(shaderID); //--- 세이더 프로그램 링크하기
-	glDeleteShader(vertexShader); //--- 세이더 객체를 세이더 프로그램에 링크했음으로, 세이더 객체 자체는 삭제 가능
-	glDeleteShader(fragmentShader);
-	glGetProgramiv(shaderID, GL_LINK_STATUS, &result); // ---세이더가 잘 연결되었는지 체크하기
-	if (!result) {
-		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
-		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
-		return false;
-	}
-	glUseProgram(shaderID);
-	return shaderID;
 }
 
 GLvoid Keyboard(unsigned char key, int x, int y) {
 	switch (key) {
 	case 'p':    // 점 그리기
+		save_draw_check[cnt] = 1;
 		break;
 	case 'l':    // 선 그리기
+		save_draw_check[cnt] = 2;
 		break;
 	case 't':    // 삼각형 그리기
+		save_draw_check[cnt] = 3;
 		break;
 	case 'r':    // 네모 그리기
+		save_draw_check[cnt] = 4;
 		break;
 	case 'w':    // 위로 이동
 		break;
@@ -131,6 +183,7 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	case 'd':    // 오른쪽으로 이동
 		break;
 	case 'c':   // 모든 도형 삭제
+		reset();
 		break;
 	case 'q':   // 프로그램 종료
 		std::cout << "프로그램을 종료합니다." << std::endl;
