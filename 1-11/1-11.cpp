71464dd162239c765a29d8e9f8a42b281a411f2d
Jeong tae yeon <jtyk119@naver.com>
2023-10-06 11:49:20 +0900

    1-11 기본 세팅 완료

---
+++ 1-11/1-11.cpp
@@ -0,0 +1,241 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+void make_vertexShaders();
+void make_fragmentShaders();
+void make_shaderProgram();
+void InitBuffer();
+void reset();
+void Time_l(int value);
+void Time_t(int value);
+void Time_r(int value);
+void Time_p(int value);
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+char* filetobuf(const char* file);
+
+GLuint shaderProgramID; //--- 세이더 프로그램 이름
+GLuint vertexShader; //--- 버텍스 세이더 객체
+GLuint fragmentShader; //--- 프래그먼트 세이더 객체
+float r[6], g[6], b[6];
+GLfloat Shape[6][15];    // 도형의 좌표
+GLfloat colors[6][15];  // 도형 꼭지점의 색상
+GLuint vao[6], vbo[12];
+
+void reset() {
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			Shape[i][j] = 0;
+			colors[i][j] = 0;
+		}
+	}
+	Shape[0][0] = Shape[1][4] = 1.0;
+	Shape[0][3] = Shape[1][1] = -1.0;
+
+	for (int i = 2; i < 6; ++i) {
+		r[i] = (rand() % 101) * 0.01, g[i] = (rand() % 101) * 0.01, b[i] = (rand() % 101) * 0.01;
+		for (int j = 0; j < 9; ++j) {
+			Shape[i][j] = 0;
+			if (j % 3 == 0) colors[i][j] = r[i];
+			else if (j % 3 == 1) colors[i][j] = g[i];
+			else if (j % 3 == 2) colors[i][j] = b[i];
+		}
+	}
+}
+
+void main(int argc, char** argv)
+{
+	srand(time(NULL));
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(200, 30);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Example");
+	glewExperimental = GL_TRUE;
+	reset();
+	glewInit();
+	make_shaderProgram();
+	InitBuffer();
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutTimerFunc(10, Time_l, 1);
+	glutTimerFunc(10, Time_t, 1);
+	glutTimerFunc(10, Time_r, 1);
+	glutTimerFunc(10, Time_p, 1);
+	glutMainLoop();
+}
+
+GLvoid drawScene() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(shaderProgramID);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shaderProgramID);
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position"); //	: 0  Shader의 'layout (location = 0)' 부분
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color");
+
+	for (int i = 0; i < 6; ++i) {
+		glBindVertexArray(vao[i]);
+		if (i <= 1) {
+			glPointSize(3);
+			glDrawArrays(GL_LINE_STRIP, 0, 2);
+		}
+	}
+
+	glEnableVertexAttribArray(PosLocation);
+	glEnableVertexAttribArray(ColorLocation);
+
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+}
+
+void InitBuffer()
+{
+	glGenVertexArrays(5, vao);
+	glGenBuffers(10, vbo);
+
+	for (int i = 0; i < 5; ++i) {
+		glBindVertexArray(vao[i]);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[i * 2]);
+		glBufferData(GL_ARRAY_BUFFER, 702 * sizeof(GLfloat), Shape[i], GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[i * 2 + 1]);
+		glBufferData(GL_ARRAY_BUFFER, 702 * sizeof(GLfloat), colors[i], GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
+	}
+}
+
+void make_shaderProgram()
+{
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+
+void make_vertexShaders()
+{
+	vertexSource = filetobuf("vertex.glsl");
+	//--- 버텍스 세이더 객체 만들기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	fragmentSource = filetobuf("fragment.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void Time_l(int value) {
+
+	glutTimerFunc(10, Time_l, 1);
+}
+
+void Time_t(int value) {
+
+	glutTimerFunc(10, Time_l, 1);
+}
+
+void Time_r(int value) {
+
+	glutTimerFunc(10, Time_l, 1);
+}
+
+void Time_p(int value) {
+
+	glutTimerFunc(10, Time_l, 1);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'l':
+		break;
+	case 't':
+		break;
+	case 'r':
+		break;
+	case 'p':
+		break;
+	case 'a':   // 리셋
+		reset();
+		InitBuffer();
+		drawScene();
+		break;
+	case 'q':   // 프로그램 종료
+		std::cout << "프로그램을 종료합니다." << std::endl;
+		glutLeaveMainLoop();
+		break;
+	}
+}
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading 
+	if (!fptr) // Return NULL on failure 
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file 
+	length = ftell(fptr); // Find out how many bytes into the file we are 
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator 
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file 
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer 
+	fclose(fptr); // Close the file 
+	buf[length] = 0; // Null terminator 
+	return buf; // Return the buffer 
+}
